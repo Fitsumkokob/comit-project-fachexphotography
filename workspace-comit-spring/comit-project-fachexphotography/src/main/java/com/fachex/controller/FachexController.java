c2be12e136a30634b1786edf63d02fee2675f94f
Fitsum Ogbatinsae <elkanah8r@gmail.com>
2022-09-16 17:55:50 -0600

    first

--- workspace-comit-spring/comit-project-fachexphotography/src/main/java/com/fachex/controller/FachexController.java
+++ workspace-comit-spring/comit-project-fachexphotography/src/main/java/com/fachex/controller/FachexController.java
@@ -2,6 +2,7 @@ package com.fachex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.fachex.service.FachexService;
 
@@ -10,4 +11,21 @@ public class FachexController {
 	
 	@Autowired
 	FachexService fachexService;
+	
+	@GetMapping("/index")
+	public String  index() {
+		return "index";
+	}
+	@GetMapping("/gallery")
+	public String Gallery() {
+		return "gallery";
+	}
+	@GetMapping("/AboutUs")
+	public String aboutUs() {
+		return "AboutUS";
+	}
+	@GetMapping("/ContactUs")
+	public String contactus() {
+		return "ContactUs";
+	}
 }
